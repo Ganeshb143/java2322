2943cf3ba62546cc7b1d2f913d52b29571200562
Ganeshb143 <113094477+Ganeshb143@users.noreply.github.com>
2022-09-22 14:17:28 +0530

    Add files via upload

---
+++ ArrayAddition.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+
+public class ArrayAddition {
+
+    public static void main(String args[]) {
+
+        // let's first create two arrays
+        int[] even = { 2, 4, 6 };
+        int[] odd = { 1, 3, 5 };
+
+        // The operator + is undefined for the argument type(s) int[],
+        // int[]
+        // int[] result = even + odd; // this will not work
+
+        int[] result = add(even, odd);
+        System.out.println("first array: " + Arrays.toString(even));
+        System.out.println("second array: " + Arrays.toString(odd));
+        System.out.println("sum of array: " + Arrays.toString(result));
+
+        // adding two array of different length in Java
+        int[] prime = { 2, 3, 5, 7 };
+        result = add(even, prime);
+        System.out.println("first array: " + Arrays.toString(even));
+        System.out.println("second array: " + Arrays.toString(prime));
+        System.out.println("sum of array: " + Arrays.toString(result));
+    }
+
+    public static int[] add(int[] first, int[] second) {
+        int length = first.length < second.length ? first.length
+                : second.length;
+        int[] result = new int[length];
+
+        for (int i = 0; i < length; i++) {
+            result[i] = first[i] + second[i];
+        }
+
+        return result;
+    }
+
+}
+
+
